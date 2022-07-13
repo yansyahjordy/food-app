97066fdbdc8bdcda3fa8d21b9c5b629707f7aa03
yansyahjordy <yansyahjordy@gmail.com>
2022-07-13 19:41:26 +0700

    feature:added post method

--- src/components/Food/TambahMenu/TambahMenu.js
+++ src/components/Food/TambahMenu/TambahMenu.js
@@ -1,7 +1,33 @@
 import React from "react";
+import { useState } from "react";
 import styles from "./TambahMenu.module.css";
+import axios from "axios";
 
 export default function TambahMenu() {
+  const [foodName, setFoodName] = useState("bakso");
+  const [foodImage, setFoodImage] = useState(
+    "https://api-test.alan.co.id/storage/food/FOOD-2722260709132316395.png"
+  );
+  const posturl = "https://api-test.alan.co.id/api/v1/food/add";
+  async function PostData() {
+    let newData = {};
+    newData.name = foodName;
+    newData.picture = foodImage;
+    newData.price = 10000;
+    newData.satuan = "piring";
+    const config = {
+      headers: {
+        "X-SECRET-TOKEN":
+          "$2a$16$TlB6hYDRMSF5HBgxImeaU.itfBOu881/lI4mSPMR0jYRnMXklQKp6",
+      },
+    };
+    await axios.post(posturl, newData, config);
+  }
+  function simpanData (){
+    PostData();
+    window.location.href="/food"
+  }
+
   return (
     <>
       <div className={styles.app}>
@@ -10,7 +36,12 @@ export default function TambahMenu() {
             <p className={styles.ptext}>Tambahkan Menu</p>
             <div>
               <p>Nama Menu</p>
-              <input></input>
+              <input
+                value={foodName}
+                onChange={(e) => {
+                  setFoodName(e.target.value);
+                }}
+              ></input>
             </div>
             <div>
               <p>Nama Menu</p>
@@ -24,7 +55,7 @@ export default function TambahMenu() {
               </div>
             </div>
             <div className={styles.btn}>
-              <button>Simpan</button>
+              <button onClick={()=>simpanData()}>Simpan</button>
             </div>
           </div>
         </div>
